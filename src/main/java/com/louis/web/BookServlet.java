93bc40e7de7f50445cf1b2794b810b73b35a963f
louis_lai <1598358615@qq.com>
2021-08-12 11:54:30 +0800

    change html pages to jsp.delete RegistServlet and LoginServlet,add BaseServlet and UserServlet,add book and resource about book

---
+++ src/main/java/com/louis/web/BookServlet.java
@@ -0,0 +1,15 @@
+package com.louis.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @赖小燚
+ * @www.louis_lai.com
+ */
+public class BookServlet extends BaseServlet{
+
+    protected void add(HttpServletRequest request, HttpServletResponse response){
+
+    }
+}
